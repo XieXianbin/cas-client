d91e9a72066198c22c2aa002d41ff8ba59cd9bf7
xiexianbin <me@xiexianbin.cn>
2016-02-27 23:17:11 +0800

    init cas_client_java

---
+++ cas_client_java/src/main/java/cn/xiexianbin/cas/client/user/UserDao.java
@@ -0,0 +1,13 @@
+package cn.xiexianbin.cas.client.user;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+@Transactional
+public interface UserDao extends CrudRepository<User, Long> {
+
+  public User findByEmail(String email);
+
+} 
